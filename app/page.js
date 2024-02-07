5c3ed1e104bb142f25330afa3d67313e45cd8a94
Ash <01tripster@gmail.com>
2024-02-07 23:54:34 +0530

    feat: add sublect list

--- app/page.js
+++ app/page.js
@@ -1,7 +1,25 @@
+import SubjectList from "@/components/SubjectListCard";
+
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1 className="text-4xl font-bold">SRM VIRTUAL LAB</h1>
+		<main className="flex min-h-screen flex-col p-10">
+			<h1 className="text-4xl font-bold text-center">SRM VIRTUAL LAB</h1>
+			<SubjectList
+				subjects={[
+					{
+						title: "DAA",
+						desc: "Design and Analysis of Algorithms",
+					},
+					{
+						title: "OODP",
+						desc: "Object Oriented Design and Programming",
+					},
+					{
+						title: "PPS",
+						desc: "Principles of Programming Languages",
+					},
+				]}
+			/>
 		</main>
 	);
 }
